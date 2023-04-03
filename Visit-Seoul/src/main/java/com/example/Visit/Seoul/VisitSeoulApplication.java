9425280146c88158b4a723811b0ffa7c1c89c534
seonghyuning <kim153684@naver.com>
2023-04-03 16:19:25 +0900

    Initial commit

---
+++ Visit-Seoul/src/main/java/com/example/Visit/Seoul/VisitSeoulApplication.java
@@ -0,0 +1,13 @@
+package com.example.Visit.Seoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitSeoulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitSeoulApplication.class, args);
+	}
+
+}
---
+++ Visit-Seoul/src/test/java/com/example/Visit/Seoul/VisitSeoulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Visit.Seoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitSeoulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
